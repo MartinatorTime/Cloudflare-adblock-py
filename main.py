a460233a31a484dabcb75267703656f8255f5267
MartinatorTime <76610490+MartinatorTime@users.noreply.github.com>
2023-04-19 23:47:53 +0300

    Ters

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import os
-
-try:
-    token = os.environ['CF_API_TOKEN']
-    email = os.environ['CF_API_EMAIL']
-    account_id = os.environ['CF_API_ACCOUNT_ID']
-except KeyError:
-    print('CF_API_TOKEN, CF_API_EMAIL or CF_API_ACCOUNT_ID environment variable not set')
-
 import CloudFlare
 import requests
-from delete import *
-from create import *
+from delete import DeleteAll
+from create import CreateAll
 
 def GetBlockList(block_list_url):
     block_list = requests.get(block_list_url).text.splitlines()
     return block_list
 
 def main():
+    try:
+        token = os.environ['CF_API_TOKEN']
+        email = os.environ['CF_API_EMAIL']
+        account_id = os.environ['CF_API_ACCOUNT_ID']
+    except KeyError:
+        print('CF_API_TOKEN, CF_API_EMAIL or CF_API_ACCOUNT_ID environment variable not set')
+        return
+
     block_list_url = "https://raw.githubusercontent.com/MartinatorTime/cloudflare-adblock/master/cloudflare/lists/pihole_domain_list.txt"
     block_list = GetBlockList(block_list_url)
     print("Block list has {} domains".format(len(block_list)))
